b6df8afe4ae25993e8cc68205884ba3d0613c9ba
toneloke <tony@guerrero.ai>
2025-01-28 14:56:34 -0800

    refactor: util files

--- apps/mx-next-client/lib/ui/input.tsx
+++ apps/mx-next-client/lib/ui/input.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { cn } from "@/lib/utils/styling/cn";
+import { cn } from "@/lib/utils/styling";
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
