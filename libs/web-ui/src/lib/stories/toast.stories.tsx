4383cf4158e9c6547cc720483d079055b7758570
Timurtek <timurbizel88@gmail.com>
2025-06-27 14:10:04 -0700

    Add(-): Fixing Cd Ci pipeline errors

--- libs/web-ui/src/lib/stories/toast.stories.tsx
+++ libs/web-ui/src/lib/stories/toast.stories.tsx
@@ -21,157 +21,165 @@ const meta: Meta = {
 export default meta;
 type Story = StoryObj;
 
+const DefaultComponent = () => {
+  const [open, setOpen] = React.useState(false);
+  return (
+    <div className="p-10">
+      <Button
+        variant="outline"
+        onClick={() => {
+          setOpen(true);
+          setTimeout(() => setOpen(false), 5000);
+        }}
+      >
+        Show Toast
+      </Button>
+      <ToastProvider duration={5000}>
+        <Toast open={open} onOpenChange={setOpen}>
+          <ToastTitle>Notification</ToastTitle>
+          <ToastDescription>
+            Your message has been sent successfully.
+          </ToastDescription>
+        </Toast>
+        <ToastViewport />
+      </ToastProvider>
+    </div>
+  );
+};
+
 export const Default: Story = {
-  render: () => {
-    const [open, setOpen] = React.useState(false);
-    return (
-      <div className="p-10">
-        <Button
-          variant="outline"
-          onClick={() => {
-            setOpen(true);
-            setTimeout(() => setOpen(false), 5000);
-          }}
-        >
-          Show Toast
-        </Button>
-        <ToastProvider duration={5000}>
-          <Toast open={open} onOpenChange={setOpen}>
-            <ToastTitle>Notification</ToastTitle>
+  render: () => <DefaultComponent />,
+};
+
+const WithActionComponent = () => {
+  const [open, setOpen] = React.useState(false);
+  return (
+    <div className="p-10">
+      <Button
+        variant="outline"
+        onClick={() => {
+          setOpen(true);
+          setTimeout(() => setOpen(false), 5000);
+        }}
+      >
+        Show Toast with Action
+      </Button>
+      <ToastProvider duration={5000}>
+        <Toast open={open} onOpenChange={setOpen}>
+          <div className="grid gap-1">
+            <ToastTitle>Undo Changes</ToastTitle>
             <ToastDescription>
-              Your message has been sent successfully.
+              Your changes have been saved. You can undo them within 5
+              seconds.
             </ToastDescription>
-          </Toast>
-          <ToastViewport />
-        </ToastProvider>
-      </div>
-    );
-  },
+          </div>
+          <ToastAction altText="Undo" onClick={() => console.log('Undo clicked')}>
+            Undo
+          </ToastAction>
+        </Toast>
+        <ToastViewport />
+      </ToastProvider>
+    </div>
+  );
 };
 
 export const WithAction: Story = {
-  render: () => {
-    const [open, setOpen] = React.useState(false);
-    return (
-      <div className="p-10">
-        <Button
-          variant="outline"
-          onClick={() => {
-            setOpen(true);
-            setTimeout(() => setOpen(false), 5000);
-          }}
-        >
-          Show Toast with Action
-        </Button>
-        <ToastProvider duration={5000}>
-          <Toast open={open} onOpenChange={setOpen}>
-            <div className="grid gap-1">
-              <ToastTitle>Undo Changes</ToastTitle>
-              <ToastDescription>
-                Your changes have been saved. You can undo them within 5
-                seconds.
-              </ToastDescription>
-            </div>
-            <ToastAction altText="Undo" onClick={() => alert('Undo clicked')}>
-              Undo
-            </ToastAction>
-          </Toast>
-          <ToastViewport />
-        </ToastProvider>
-      </div>
-    );
-  },
+  render: () => <WithActionComponent />,
+};
+
+const DestructiveComponent = () => {
+  const [open, setOpen] = React.useState(false);
+  return (
+    <div className="p-10">
+      <Button
+        variant="outline"
+        onClick={() => {
+          setOpen(true);
+          setTimeout(() => setOpen(false), 5000);
+        }}
+      >
+        Show Destructive Toast
+      </Button>
+      <ToastProvider duration={5000}>
+        <Toast variant="destructive" open={open} onOpenChange={setOpen}>
+          <div className="grid gap-1">
+            <ToastTitle>Error</ToastTitle>
+            <ToastDescription>
+              There was a problem with your request. Please try again.
+            </ToastDescription>
+          </div>
+          <ToastAction
+            altText="Try again"
+            onClick={() => console.log('Try again clicked')}
+          >
+            Try again
+          </ToastAction>
+        </Toast>
+        <ToastViewport />
+      </ToastProvider>
+    </div>
+  );
 };
 
 export const Destructive: Story = {
-  render: () => {
-    const [open, setOpen] = React.useState(false);
-    return (
-      <div className="p-10">
-        <Button
-          variant="outline"
-          onClick={() => {
-            setOpen(true);
-            setTimeout(() => setOpen(false), 5000);
-          }}
+  render: () => <DestructiveComponent />,
+};
+
+const MultiplePrioritiesComponent = () => {
+  const [defaultOpen, setDefaultOpen] = React.useState(false);
+  const [destructiveOpen, setDestructiveOpen] = React.useState(false);
+
+  const showDefault = () => {
+    setDefaultOpen(true);
+    setTimeout(() => setDefaultOpen(false), 5000);
+  };
+
+  const showDestructive = () => {
+    setDestructiveOpen(true);
+    setTimeout(() => setDestructiveOpen(false), 5000);
+  };
+
+  const showBoth = () => {
+    showDefault();
+    setTimeout(showDestructive, 1000);
+  };
+
+  return (
+    <div className="p-10 space-x-4">
+      <Button variant="outline" onClick={showDefault}>
+        Regular Toast
+      </Button>
+      <Button variant="destructive" onClick={showDestructive}>
+        Error Toast
+      </Button>
+      <Button onClick={showBoth}>Show Both</Button>
+      <ToastProvider duration={5000}>
+        <Toast open={defaultOpen} onOpenChange={setDefaultOpen}>
+          <ToastTitle>Success</ToastTitle>
+          <ToastDescription>Your changes have been saved.</ToastDescription>
+        </Toast>
+        <Toast
+          variant="destructive"
+          open={destructiveOpen}
+          onOpenChange={setDestructiveOpen}
         >
-          Show Destructive Toast
-        </Button>
-        <ToastProvider duration={5000}>
-          <Toast variant="destructive" open={open} onOpenChange={setOpen}>
-            <div className="grid gap-1">
-              <ToastTitle>Error</ToastTitle>
-              <ToastDescription>
-                There was a problem with your request. Please try again.
-              </ToastDescription>
-            </div>
-            <ToastAction
-              altText="Try again"
-              onClick={() => alert('Try again clicked')}
-            >
-              Try again
-            </ToastAction>
-          </Toast>
-          <ToastViewport />
-        </ToastProvider>
-      </div>
-    );
-  },
+          <ToastTitle>Error</ToastTitle>
+          <ToastDescription>
+            There was a problem with your request.
+          </ToastDescription>
+          <ToastAction
+            altText="Try again"
+            onClick={() => console.log('Try again clicked')}
+          >
+            Try again
+          </ToastAction>
+        </Toast>
+        <ToastViewport />
+      </ToastProvider>
+    </div>
+  );
 };
 
 export const MultiplePriorities: Story = {
-  render: () => {
-    const [defaultOpen, setDefaultOpen] = React.useState(false);
-    const [destructiveOpen, setDestructiveOpen] = React.useState(false);
-
-    const showDefault = () => {
-      setDefaultOpen(true);
-      setTimeout(() => setDefaultOpen(false), 5000);
-    };
-
-    const showDestructive = () => {
-      setDestructiveOpen(true);
-      setTimeout(() => setDestructiveOpen(false), 5000);
-    };
-
-    const showBoth = () => {
-      showDefault();
-      setTimeout(showDestructive, 1000);
-    };
-
-    return (
-      <div className="p-10 space-x-4">
-        <Button variant="outline" onClick={showDefault}>
-          Regular Toast
-        </Button>
-        <Button variant="destructive" onClick={showDestructive}>
-          Error Toast
-        </Button>
-        <Button onClick={showBoth}>Show Both</Button>
-        <ToastProvider duration={5000}>
-          <Toast open={defaultOpen} onOpenChange={setDefaultOpen}>
-            <ToastTitle>Success</ToastTitle>
-            <ToastDescription>Your changes have been saved.</ToastDescription>
-          </Toast>
-          <Toast
-            variant="destructive"
-            open={destructiveOpen}
-            onOpenChange={setDestructiveOpen}
-          >
-            <ToastTitle>Error</ToastTitle>
-            <ToastDescription>
-              There was a problem with your request.
-            </ToastDescription>
-            <ToastAction
-              altText="Try again"
-              onClick={() => alert('Try again clicked')}
-            >
-              Try again
-            </ToastAction>
-          </Toast>
-          <ToastViewport />
-        </ToastProvider>
-      </div>
-    );
-  },
+  render: () => <MultiplePrioritiesComponent />,
 };
